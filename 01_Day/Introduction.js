b74cdaf16cd82f631d8626652458d722f49adce3
Htet Paing Kyaw <htetpaingk50@gmail.com>
2022-03-26 20:28:18 +0630

    Day02DataTypes

---
+++ 01_Day/Introduction.js
@@ -0,0 +1,43 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*
+comment can make code Readable
+easy to reuse and informative
+*/
+
+let name = "Htet Paing Kyaw";
+let isMarried = false;
+let empty = null;
+let girlfriend;
+
+console.log(typeof(name));
+console.log(typeof(isMarried));
+console.log(typeof(empty));
+console.log(typeof(girlfriend));
+
+let student01;
+let student02;
+let student03;
+let student04;
+
+let teacher01 = "Maung Maung";
+let teacher02 = "Kyaw Kyaw";
+let teacher03 = "Su Su";
+let teacher04 = "Htet Htet";
+
+let firstName = "Htet Paing";
+let lastName = "Kyaw";
+let maritalStatus = "Single";
+let country = "Myanmar";
+let age = 20;
+
+let firstNamee = "Htet Paing" , lastNamee = "Kyaw" , maritalStatuss = "Single" , countryy ="Myanmar" , agee = 20;
+
+let myAge = 20;
+let yourAge = 25;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
+
+//Done Day 01
+
